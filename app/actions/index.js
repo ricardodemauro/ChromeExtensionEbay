0e0aaabf79bf22397112b265eee766a1768f82dd
Ricardo Mauro <ricardo.demauro@outlook.com>
2018-03-13 14:36:41 -0300

    adding azure function and redirect to new url

--- app/actions/index.js
+++ app/actions/index.js
@@ -67,7 +67,7 @@ export const clearData = () => {
 export const fetchDetailAsset = name => dispatch => {
     dispatch(fetchingStatusAsset(name))
 
-    const uri = `http://localhost:3000/asset/${name}`
+    const uri = `https://ebayappextension.azurewebsites.net/api/productjs?code=nMKps7SU90VN16pg4KmGyZ77IwXtlIUR06w1guRYkskLRhUp0Ebhqg==&name=${name}`
     const opts = {
         method: 'GET',
         headers: { 'Access-Control-Allow-Origin': '*' },
@@ -84,12 +84,13 @@ export const fetchDetailAsset = name => dispatch => {
         })
         .catch(err => {
             dispatch(errorFetch(err))
+            dispatch(endFetch())
+            dispatch(fetchDetailAsset(name))
         })
 }
 
 export const fetchSlug = data => dispatch => {
-    //const uri = `https://autosug.ebay.com/autosug?kwd=${data}&_jgr=1&sId=0&_ch=0&callback=nil`
-    const uri = `http://localhost:3000/slug/${data}`
+    const uri = `https://ebayappextension.azurewebsites.net/api/slugjs?code=Eug0rYa1bHQMMmoOAQ1mKZhWdwVxnk/NuBmDWszN0aaH63OhNdmwpg==&name=${data}`
     
     const opts = {
         method: 'GET',
@@ -107,6 +108,8 @@ export const fetchSlug = data => dispatch => {
         })
         .catch(err => {
             dispatch(errorFetch(err))
+            dispatch(endFetch())
+            dispatch(fetchSlug(data))
         })
 }
 
--- app/containers/SearchApp.js
+++ app/containers/SearchApp.js
@@ -22,7 +22,7 @@ const styles = theme => ({
     },
 });
 
-const letters = ['a', 'b']
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z']
 
 class SearchApp extends Component {
     static propTypes = {
